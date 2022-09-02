ffcdcfc48051ef755f5df4a5fd3a5f89e1c08838
Fathurrahman <fathur.ashter15@gmail.com>
2022-09-02 09:27:08 +0800

    first commit

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -23,51 +23,15 @@ class Auth extends CI_Controller
 
 	private function _loginAction()
 	{
-		$email = $this->input->post("user_email");
+		$username = $this->input->post("user_email");
 		$password = $this->input->post("user_password");
 
 		// cek apakah dengan email yang di input ada
-		$userData = $this->db->get_where("users", ["user_email" => $email])->row_array();
+		$userData = $this->db->get_where("users", ["username" => $username])->row_array();
 		if ($userData) {
 			// cek apakah password yang dimasukkan benar
 			if (password_verify($password, $userData["user_password"])) {
-				$data = [
-					"user_name" => $userData["user_name"],
-					"user_email" => $userData["user_email"],
-					"user_phone" => $userData["user_phone"],
-					"user_address" => $userData["user_address"],
-					"user_avatar" => $userData["user_avatar"],
-					"user_role" => $userData["user_role"],
-					"created_at" => $userData["created_at"],
-					"id_user" => $userData["id_user"],
-					"user_password" => $userData["user_password"],
-
-				];
-				$tmp = $this->db->select('w1.id as idwil, w1.nama as wilnama, w1.level as willevel')
-					->from('users')
-					->join('wilayah w1', 'w1.id = users.wilayah')
-					->where('users.id_user', $userData['id_user'])->get()->row_array();
-
-				if($data['user_role'] == 'admin'){
-				
-				}else{
-					$gudang = $this->db->select('gudang.*, wilayah.nama as wilayah_gudang, wilayah.level as level_wilayah_gudang')
-						->from('users')
-						->join('gudang', 'gudang.id = users.gudang')
-						->join('wilayah', 'wilayah.id = gudang.wilayah')
-						->where('users.id_user', $userData['id_user'])
-						->get()->result();
-						
-				}
-				$data['gudang'] = [];
-				if(!empty($gudang)){
-					foreach($gudang as $v)
-						$data['gudang'][] = (array) $v;
-				}
-				if(!empty($tmp))
-					$data = array_merge($data, $tmp);
-
-				$this->session->set_userdata('login' . '_' . APPNAME, $data);
+				$this->session->set_userdata('login' . '_' . APPNAME, $userData);
 				redirect('dashboard');
 			} else {
 				$this->session->set_flashdata('message', ['message' => 'Password kamu salah', 'type' => 'danger']); 
